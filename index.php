13405a4084099309155fd98927fec44969cbf140
Ulises Rodriguez <ing.ulisesrodriguez@gmail.com>
2012-08-02 14:12:40 -0500

    Maquetación del sitio viajesconsentido.cl
    Maquetación web del sitio viajesconsentido.cl
    
    Uso de:
    Html, CSS, jQuery

---
+++ index.php
@@ -0,0 +1,550 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<html>
+	<head>
+		<title>Nuevo Diseño</title>
+		<link href="css/estilo.css" rel="stylesheet" type="text/css">
+	</head>
+	
+	<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+	<script type="text/javascript" src="js/jquery.jcarousel.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/carousel.css" />
+
+<script type="text/javascript">
+
+jQuery(document).ready(function() {
+    jQuery('#mycarousel').jcarousel({
+    	wrap: 'circular'
+    });
+});
+
+</script>
+	
+<body>
+
+	<center>
+		
+		
+		<div class="top_login">
+			<div class="contenedor_login">
+				
+				
+				
+				<div class="titulo_ingreso">
+					Ingreso de Voluntarios
+				</div>
+				<div class="caja_rut">
+					<div class="texto_antes_input">
+						Rut
+					</div>
+					<input type="text" class="input_login">
+					<div class="suelta"></div>
+				</div>
+				<div class="caja_rut">
+					<div class="texto_antes_input">
+						Clave
+					</div>
+					<input type="password" class="input_login">
+					<div class="suelta"></div>
+				</div>
+				<a href="#" class="ingresar">
+					Ingresar
+				</a>
+					
+				<div class="suelta"></div>
+				
+				
+			</div>
+		</div>
+		
+		<div class="barra_abrir_login">
+			<a href="">Login</a>
+		</div>
+		
+		<div class="suelta"></div>
+		<div class="top_sitio">
+			<div class="contenedor_top" align="left">
+				<div class="top_logo">
+					<img src="images/home/muestra_top_logo.png" alt="">
+				</div>
+				<div class="bienvenida_usuario">
+					Bienvenido <span class="usuario_bienvenida"> Usuario</span>
+				</div>
+			</div>
+		</div>
+		
+		<div class="todo_que_seccion">
+			<div class="que_seccion">
+				Inicio
+			</div>
+		</div>
+		
+		
+		<!-- Comienza Contenido -->
+		
+		<div class="tutti_contenido">
+			<div class="contenedor_contenidos">
+			
+				<div class="menu">
+					<ul>
+						<li>
+							<a href="">Sorteos</a>
+						</li>
+						<li>
+							<a href="">Premios</a>
+						</li>
+						<li class="selected">
+							Consejos
+						</li>
+						<li>
+							<a href="">Preguntas</a>
+						</li>
+						<li>
+							<a href="">Recomienda a un Amigo</a>
+						</li>
+						<div class="suelta"></div>
+					</ul>
+				</div>
+				
+				<div class="suelta"></div>
+				<div class="caja_contenido_institucion" align="center">
+					<div class="top_caja_institucion"></div>
+					<div class="mdm_caja_institucion">
+						<img src="images/generales/muestra_banner_institucion.png" style="clear: both" alt="" />
+					</div>
+					<div class="btm_caja_institucion"></div>
+				</div>
+				
+				<div class="noticias_home">
+					<div id="wrap">
+    					<ul id="mycarousel" class="jcarousel-skin-tango">
+    						
+    						
+    						<!-- Una Noticia! -->
+    						<li>
+    							<div class="caja_info" style="background-image: url('images/noticias/muestra_autos.png'); background-color: #fff;">
+    								<div class="fecha">
+    									<div class="noticia_dia">1</div>
+    									<div class="noticia_mes">JUN</div>
+    								</div>
+    								<div class="titulo_fecha">
+    									Titulo de la Noticia
+    								</div>
+    							</div>
+    							
+    							<div class="txt_noticia">
+    								Aliquam gravida nunc eu lectus aliquet non scelerisque turpis mollis. Morbi non lobortis odio. Curabitur eleifend placerat sapien, eget sollicitudin nisi sagittis a. Aliquam eu metus lectus, sit amet aliquam ipsum. Suspendisse condimentum ipsum justo, id mattis tortor. Nam hendrerit adipiscing augue, ut suscipit leo sollicitudin id. Duis non lectus massa. Morbi eu lacus vel libero ultrices gravida et at turpis. Mauris tincidunt arcu a nulla varius eget bibendum sapien aliquet. Mauris cursus justo nec mauris consectetur et facilisis enim imperdiet.
+    							</div>
+    							
+    						</li>
+    						<!-- Cierre Noticia -->
+    						
+    						<!-- Una Noticia! -->
+    						<li>
+    							<div class="caja_info" style="background-image: url('images/noticias/muestra_autos.png'); background-color: #fff;">
+    								<div class="fecha">
+    									<div class="noticia_dia">22</div>
+    									<div class="noticia_mes">Sept</div>
+    								</div>
+    								<div class="titulo_fecha">
+    									Titulo de la Noticia
+    								</div>
+    							</div>
+    							
+    							<div class="txt_noticia">
+    								Aliquam gravida nunc eu lectus aliquet non scelerisque turpis mollis. Morbi non lobortis odio. Curabitur eleifend placerat sapien, eget sollicitudin nisi sagittis a. Aliquam eu metus lectus, sit amet aliquam ipsum. Suspendisse condimentum ipsum justo, id mattis tortor. Nam hendrerit adipiscing augue, ut suscipit leo sollicitudin id. Duis non lectus massa. Morbi eu lacus vel libero ultrices gravida et at turpis. Mauris tincidunt arcu a nulla varius eget bibendum sapien aliquet. Mauris cursus justo nec mauris consectetur et facilisis enim imperdiet.
+    							</div>
+    							
+    						</li>
+    						<!-- Cierre Noticia -->
+    						
+    						
+						</ul>
+						<div class="suelta"></div>
+					</div>
+					<div class="suelta"></div>
+				</div>
+			
+				<div class="info_general">
+					<div class="caja_info_general">
+						<div class="ranking">
+							<div class="titulo_caja_info"></div>
+							
+							<div class="info_ranking">
+								<div class="titulo_ranking">
+									Mejor Captador
+								</div>
+								<div class="numero_1">
+									<div class="numero">
+										1
+									</div>
+								</div>
+								<div class="detalle_1">
+									<div class="mandatos">
+										X Mandatos, $300.000
+									</div>
+									<div class="nombre_1">
+										Joel Manuel Acevedo
+									</div>
+								</div>
+								<div class="suelta"></div>
+							</div>
+							
+							<div class="info_ranking">
+								<div class="titulo_ranking">
+									Mejor Equipo
+								</div>
+								<div class="numero_1">
+									<div class="numero">
+										1
+									</div>
+								</div>
+								<div class="detalle_1">
+									<div class="mandatos">
+										X Mandatos, $300.000
+									</div>
+									<div class="nombre_1">
+										Cuerpo de Bomberos
+									</div>
+								</div>
+								<div class="suelta"></div>
+							</div>
+							
+							<div class="ver_mas">
+								<a href="">
+									Ver Más
+								</a>
+							</div>
+							
+						</div>
+					</div>
+					<div class="caja_info_general">
+						<div class="captaciones">
+						
+							<div class="titulo_caja_info" style="height: 37px;"></div>
+							
+							<div class="tus_captaciones">
+								<div class="txt_capt" style="padding-right: 15px;">
+									Captaciones
+								</div>
+								<div class="num_capt">
+									23
+								</div>
+								<div class="txt_capt">
+									Tienes
+								</div>
+								<div class="suelta"></div>
+							</div>
+							
+							<div class="info_captaciones">							
+							
+								<div class="contengo_captacion">
+									<div class="titulo_capt">
+										Gánale A
+									</div>
+									<div class="nombre">
+										Joel Manuel Acevedo
+									</div>
+									<div class="num_captaciones">
+										X Captaciones
+									</div>
+									<div class="suelta"></div>
+								</div>
+								
+								<div class="contengo_captacion">
+									<div class="titulo_capt">
+										Gánale A
+									</div>
+									<div class="nombre">
+										Joel Manuel Acevedo
+									</div>
+									<div class="num_captaciones">
+										X Captaciones
+									</div>
+									<div class="suelta"></div>
+								</div>
+							
+								<div class="ver_mas">
+									<a href="">
+										Ver Más
+									</a>
+								</div>
+							
+							</div>
+							
+						</div>
+					</div>
+					<div class="caja_info_general">
+						<div class="cupones">
+						
+							<div class="titulo_caja_info"></div>
+							
+							<div class="titulo_cupones">
+								Tienes
+							</div>
+							<div class="caja_cupones">
+								<div class="num_cupones">
+									0
+								</div>
+								<div class="txt_cupones">
+									Cupones
+								</div>
+							</div>
+							
+						</div>
+					</div>
+					<div class="suelta"></div>
+				</div>
+			
+				<div class="seccion_inferior">
+					<div class="muro">
+						<div class="top_muro"></div>
+						<div class="mdm_muro" align="left">
+							
+							<div class="titulo_muro"></div>
+							<div class="subtitulo_muro">
+								Escribe un mensaje para publicar en el muro
+							</div>
+							
+							<div class="tutti_escribe_muro">
+								<div class="escribir_en_muro">
+									<div class="quien_escribe">
+										<div class="fecha_muro">
+											Ahora
+										</div>
+										<div class="autor_muro">
+											Juan
+										</div>
+									</div>
+									<div class="mensaje_a_muro">
+										<!-- eaea -->
+										<textarea class="ingresa_muro">Ingresa tu Comentario</textarea>
+									</div>
+									<div class="suelta"></div>
+									<input type="button" class="publicar_en_muro" value="Publicar" />
+									<div class="suelta"></div>
+								</div>
+							<div class="suelta"></div>
+							</div>
+							
+							<!-- Hace el for con los dos siguientes, para que cambien su posición. -->
+							
+							<div class="un_mensaje mensaje_derecha">
+								<div class="mensaje_a_muro">
+									Eaeaeaeaeaeaeaea
+								</div>
+								<div class="suelta"></div>
+								<div class="quien_fue quien_fue_derecha">
+									<div class="fecha_muro">
+										Ahora
+									</div>
+									<div class="autor_muro">
+										Juan
+									</div>
+									<div class="suelta"></div>
+								</div>
+								<div class="suelta"></div>
+							</div>
+
+							<div class="un_mensaje mensaje_izquierda">
+								<div class="mensaje_a_muro">
+									Eaeaeaeaeaeaeaea
+								</div>
+								<div class="suelta"></div>
+								<div class="quien_fue">
+									<div class="fecha_muro">
+										Ahora
+									</div>
+									<div class="autor_muro">
+										Juan
+									</div>
+									<div class="suelta"></div>
+								</div>
+								<div class="suelta"></div>
+							</div>
+
+							
+							<!-- Hasta acá para que cambien su posición. -->
+							
+							
+						</div>
+						<div class="btm_muro"></div>
+					</div>
+					<div class="descargas">
+					
+						<div class="top_descarga" align="right">
+							<div class="titulo_descargas"></div>
+						</div>
+						<div class="mdm_descarga">
+							
+							<div class="tipo_descarga">
+								Imágenes
+							</div>
+								
+								
+								<!-- Una Descarga -->
+								<div class="una_descarga">
+									<div class="top_la_descarga"></div>
+									<div class="mdm_la_descarga">
+										Flyer: Flyer de la Campaña
+										<div class="suelta"></div>
+									</div>
+									<div class="btm_la_descarga"></div>
+								</div>
+								<div class="suelta"></div>
+								<!--Cierre Descarga -->
+								<!-- Una Descarga -->
+								<div class="una_descarga">
+									<div class="top_la_descarga"></div>
+									<div class="mdm_la_descarga">
+										Flyer: Flyer de la Campaña
+										<div class="suelta"></div>
+									</div>
+									<div class="btm_la_descarga"></div>
+								</div>
+								<div class="suelta"></div>
+								<!--Cierre Descarga -->
+								<!-- Una Descarga -->
+								<div class="una_descarga">
+									<div class="top_la_descarga"></div>
+									<div class="mdm_la_descarga">
+										Flyer: Flyer de la Campaña
+										<div class="suelta"></div>
+									</div>
+									<div class="btm_la_descarga"></div>
+								</div>
+								<div class="suelta"></div>
+								<!--Cierre Descarga -->
+							
+							<div class="tipo_descarga">
+								Documentos
+							</div>
+								<!-- Una Descarga -->
+								<div class="una_descarga">
+									<div class="top_la_descarga"></div>
+									<div class="mdm_la_descarga">
+										Flyer: Flyer de la Campaña
+										<div class="suelta"></div>
+									</div>
+									<div class="btm_la_descarga"></div>
+								</div>
+								<div class="suelta"></div>
+								<!--Cierre Descarga -->
+							
+						</div>
+						<div class="btm_descarga"></div>
+					
+					</div>
+					<div class="suelta"></div>
+				</div>
+			
+			</div>
+		</div>
+		
+		<div class="footer">
+			<div class="contenido_footer">
+				<div class="footer_izq">
+					
+					<div class="titulo_footer">
+						<div class="caja_footer_1"></div>
+						<div class="caja_footer_2">
+							Título de Campaña
+						</div>
+						<div class="caja_footer_3"></div>
+					</div>
+					
+					
+					<div class="datos_footer">
+						
+						<!-- Un Dato Footer -->
+						<div class="caja_dato_footer">
+							<div class="el_dato_footer">
+								Fono
+							</div>
+							<div class="el_valor_footer">
+								(02) 123 45 67
+							</div>
+						</div>
+						<!-- Cierre Dato Footer -->
+						<!-- Un Dato Footer -->
+						<div class="caja_dato_footer">
+							<div class="el_dato_footer">
+								Mail
+							</div>
+							<div class="el_valor_footer">
+								mail@email.com
+							</div>
+						</div>
+						<!-- Cierre Dato Footer -->
+
+					</div>
+										
+					
+				</div>
+				<div class="footer_der">
+					
+					<!-- Un Boton de Red -->
+					<div class="un_boton_footer">
+						<div class="boton_footer_1"></div>
+						<div class="boton_footer_2">
+							<div class="interior_boton_footer_2">
+								<a href="">
+									<img src="images/footer/facebook.png" alt="" />
+								</a>
+							</div>
+						</div>
+						<div class="boton_footer_3"></div>
+					</div>
+					<!-- Cierre Boton de Red -->
+					
+					<!-- Un Boton de Red -->
+					<div class="un_boton_footer">
+						<div class="boton_footer_1"></div>
+						<div class="boton_footer_2">
+							<div class="interior_boton_footer_2">
+								<a href="">
+									<img src="images/footer/youtube.png" alt="" />
+								</a>
+							</div>
+						</div>
+						<div class="boton_footer_3"></div>
+					</div>
+					<!-- Cierre Boton de Red -->
+					
+					<!-- Un Boton de Red -->
+					<div class="un_boton_footer">
+						<div class="boton_footer_1"></div>
+						<div class="boton_footer_2">
+							<div class="interior_boton_footer_2">
+								<a href="">
+									<img src="images/footer/flickr.png" alt="" />
+								</a>
+							</div>
+						</div>
+						<div class="boton_footer_3"></div>
+					</div>
+					<!-- Cierre Boton de Red -->
+					
+					<!-- Un Boton de Red -->
+					<div class="un_boton_footer">
+						<div class="boton_footer_1"></div>
+						<div class="boton_footer_2">
+							<div class="interior_boton_footer_2">
+								<a href="">
+									<img src="images/footer/twitter.png" alt="" />
+								</a>
+							</div>
+						</div>
+						<div class="boton_footer_3"></div>
+					</div>
+					<!-- Cierre Boton de Red -->
+					
+					
+				</div>
+			</div>
+		</div>
+		
+	</center>
+
+</body>
+</html>
